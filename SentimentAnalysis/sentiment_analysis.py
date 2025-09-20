78e63dc6eb56664f6a87b9fea3a90c220792cdd5
civilcodebot <lk9048+git@gmail.com>
2025-09-20 04:00:22 -0400

    lab done

---
+++ SentimentAnalysis/sentiment_analysis.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+def sentiment_analyzer(text_to_analyse):
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    headers = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = myobj, headers=headers)
+    formatted_response = json.loads(response.text)
+
+    if response.status_code == 200:
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    elif response.status_code == 500:
+        label = None
+        score = None
+
+    return {'label': label, 'score': score}
+
+if __name__ == "__main__":
+    print(sentiment_analyzer("I love this new technology"))
